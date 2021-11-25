bbd8b823a1b5a2e7f7335bfefeedee4673e0e8b3
elavagni <>
2021-11-25 14:19:12 -0600

    Add helper and configuration files

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,14 +7,6 @@ import { async } from 'regenerator-runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
