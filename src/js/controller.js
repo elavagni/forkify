c40854a5da898de971eaf665d4ddfd8f7ac40e75
elavagni <>
2021-11-27 15:59:26 -0600

    Increase or decrease servings per recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -26,6 +26,8 @@ const controlRecipes = async function () {
 
     //Render recipe
     recipeView.render(model.state.recipe);
+
+    console.log(model.state.recipe);
   } catch (error) {
     recipeView.renderError();
   }
@@ -60,8 +62,18 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  //update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  //update the recipe view
+  //Render recipe
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
