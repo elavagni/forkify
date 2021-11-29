5b0fd973265e8616e23fc2dd71216457e0beb23c
elavagni <>
2021-11-28 18:47:34 -0600

    Create PreviewView to encapsulate duplicate code

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import View from './view.js';
+import previewView from './previewView';
 
 class BookmarkView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
@@ -6,28 +7,9 @@ class BookmarkView extends View {
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it ;)';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-    return `  
-        <li class="preview">
-            <a class="preview__link ${
-              result.id === id ? 'preview__link--active' : ''
-            }" href="#${result.id}">
-                <figure class="preview__fig">
-                 <img src="${result.image}" alt="${result.title}" />
-                </figure>
-                <div class="preview__data">
-                    <h4 class="preview__title">${result.title}</h4>
-                    <p class="preview__publisher">${
-                      result.publisher
-                    }</p>                   
-                </div>
-            </a>
-        </li>
-        `;
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
--- src/js/views/view.js
+++ src/js/views/view.js
@@ -3,12 +3,17 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
+
+    if (!render) {
+      return markup;
+    }
+
     //Remove existing markup (spinner)
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
