05259cbd49a2530609bd55a9c3471194eab907a4
elavagni <>
2021-12-05 14:16:42 -0600

    Create searchResultModel and refactor code

---
+++ src/js/models/searchResultsModel.js
@@ -0,0 +1,32 @@
+import { async } from 'regenerator-runtime';
+import { API_KEY, API_URL, RESULTS_PER_PAGE } from '../config.js';
+import { AJAX } from '../helper.js';
+import { state } from './appStateModel';
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await AJAX(`${API_URL}?search=${query}&key=${API_KEY}`);
+
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+        ...(recipe.key && { key: recipe.key }),
+      };
+    });
+    state.search.page = 1;
+  } catch (error) {
+    //Temp error handling
+    console.error(`${error} 💥`);
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
