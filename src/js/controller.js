f32e8a2c566f65609506a0a0e950f4cbed572c50
elavagni <>
2021-12-05 13:45:47 -0600

    Create recipeModel and refactor code

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './models/model.js';
+import * as recipeModel from './models/recipeModel.js';
 import { state } from './models/appStateModel.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
@@ -30,7 +31,7 @@ const controlRecipes = async function () {
     bookmarksView.update(state.bookmarks);
 
     //Loading recipe
-    await model.loadRecipe(id);
+    await recipeModel.loadRecipe(id);
 
     //Render recipe
     recipeView.render(state.recipe);
@@ -70,7 +71,7 @@ const controlPagination = function (goToPage) {
 
 const controlServings = function (newServings) {
   //update the recipe servings (in state)
-  model.updateServings(newServings);
+  recipeModel.updateServings(newServings);
 
   //update the recipe view
   //Render recipe
@@ -102,7 +103,7 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderSpinner();
 
     //upload recipe data
-    await model.uploadRecipe(newRecipe);
+    await recipeModel.uploadRecipe(newRecipe);
 
     //Render recipe
     recipeView.render(state.recipe);
