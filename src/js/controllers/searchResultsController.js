1211b58ab0c24ef56b5622af141876e0576b31c8
elavagni <>
2021-12-05 18:18:45 -0600

    Create searchResultsController and refactor code

---
+++ src/js/controllers/searchResultsController.js
@@ -0,0 +1,46 @@
+import * as searchResultsModel from '../models/searchResultsModel.js';
+import { state } from '../models/appStateModel.js';
+
+import searchView from '../views/searchView.js';
+import resultsView from '../views/resultsView.js';
+import paginationView from '../views/paginationView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+const search = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await searchResultsModel.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(searchResultsModel.getSearchResultsPage(1));
+
+    //Show pagination
+    paginationView.render(state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderPaginatedResults = function (goToPage) {
+  // Render new results
+  resultsView.render(searchResultsModel.getSearchResultsPage(goToPage));
+
+  //Show new pagination buttons
+  paginationView.render(state.search);
+};
+
+export const init = function () {
+  searchView.addHandlerSearch(search);
+  paginationView.addHandlerClick(renderPaginatedResults);
+};
+
+init();
